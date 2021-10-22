9b58816fe3fa1de2d43ab821a6acac7c220c4a7e
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-22 15:29:15 +0000

    create search component

---
+++ rest-countries-api-with-color-theme-switcher/src/components/Search.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface ISearchProps {}
+
+const Search: React.FunctionComponent<ISearchProps> = (props) => {
+  return <h1>Search</h1>;
+};
+
+export default Search;
