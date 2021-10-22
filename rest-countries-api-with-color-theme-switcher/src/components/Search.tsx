d34b07cacac23503ee0e8cba4708ed3f833b4e00
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-22 16:04:46 +0000

    add display flex to the home

--- rest-countries-api-with-color-theme-switcher/src/components/Search.tsx
+++ rest-countries-api-with-color-theme-switcher/src/components/Search.tsx
@@ -9,7 +9,11 @@ const Search: React.FunctionComponent<ISearchProps> = (props) => {
     <div className="search__container">
       <div className="search">
         <i className="fas fa-search"></i>
-        <input placeholder="Search for a country..." type="text" />
+        <input
+          placeholder="Search for a country..."
+          type="text"
+          name="search"
+        />
       </div>
       <div className="filter">
         <input placeholder="" type="text" />
