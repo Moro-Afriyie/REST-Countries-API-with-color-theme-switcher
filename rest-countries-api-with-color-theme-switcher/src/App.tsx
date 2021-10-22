df4d907503beca29e4055d75abcdedc2701aa964
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-22 11:11:46 +0000

    import the components in the app.tsx file

--- rest-countries-api-with-color-theme-switcher/src/App.tsx
+++ rest-countries-api-with-color-theme-switcher/src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./App.scss";
+import Details from "./components/Details";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <Details />
+    </div>
+  );
 }
 
 export default App;
