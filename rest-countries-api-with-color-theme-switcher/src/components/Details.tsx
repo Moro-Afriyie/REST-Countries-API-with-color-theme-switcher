49e7f5fcf75477fd9e0ab65a5747929dfbeffc7b
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-22 11:08:14 +0000

    create the various components for the projects

---
+++ rest-countries-api-with-color-theme-switcher/src/components/Details.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface IDetailsProps {}
+
+const Details: React.FunctionComponent<IDetailsProps> = (props) => {
+  return <h1>Details</h1>;
+};
+
+export default Details;
---
+++ rest-countries-api-with-color-theme-switcher/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface IHeaderProps {}
+
+const Header: React.FunctionComponent<IHeaderProps> = (props) => {
+  return <h1>Header</h1>;
+};
+
+export default Header;
