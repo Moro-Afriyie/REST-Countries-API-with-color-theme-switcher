7036871544d6bdaad26a6ef253df9a15c5e21144
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-11-08 16:08:34 -0100

    theme swithing done

--- rest-countries-api-with-color-theme-switcher/src/App.tsx
+++ rest-countries-api-with-color-theme-switcher/src/App.tsx
@@ -4,7 +4,7 @@ import Details from "./components/Details";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import { Route, Switch } from "react-router-dom";
-import { Provider, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Theme } from "./interfaces/interface";
 
 const url = "https://restcountries.com/v2/all";
