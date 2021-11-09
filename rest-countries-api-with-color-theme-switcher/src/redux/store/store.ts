18f88c24689e5886e876f7f86908a4b3409d09dd
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-11-09 08:06:51 -0100

    import composeWithDevTools in order to use the redux-devtools-extension

--- rest-countries-api-with-color-theme-switcher/src/redux/store/store.ts
+++ rest-countries-api-with-color-theme-switcher/src/redux/store/store.ts
@@ -1,5 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { rootReducer } from "../reducers/rootReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
