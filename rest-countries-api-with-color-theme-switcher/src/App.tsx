9bbf6dcf1eaa30d1fef4d559cecf5f5907fdb283
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-11-14 11:15:19 -0100

    add a useEffect function to automatically detect if the user uses der is using darkmode and set the theme to dark

--- rest-countries-api-with-color-theme-switcher/src/App.tsx
+++ rest-countries-api-with-color-theme-switcher/src/App.tsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.scss";
 import Details from "./components/Details";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import { Route, Switch } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { IRootState } from "./redux/reducers/rootReducer";
+import { toggleTheme } from "./redux/actions/toggleThemeActions";
 
 function App() {
   const toggle = useSelector(
     (state: IRootState) => state.toggleTheme.toggleTheme
   );
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const matched = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    if (matched) dispatch(toggleTheme());
+  }, []);
 
   return (
     <div className={`container ${toggle ? "dark" : "light"}`}>
